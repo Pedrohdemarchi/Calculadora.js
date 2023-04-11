97eae61848e94267236841116d2973711e1f0fb9
Pedro De Marchi <pphhssddmm@gmail.com>
2023-04-11 18:00:13 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator{
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+
+    //add digit to calculator screen
+
+    addDigit(digit){
+
+        //check if current operation already has a dot
+        if(digit === "." && this.currentOperationText.innerText.includes(".")){
+            return
+        }
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    //process all operations
+    processOperation(operation){
+        let operationValue;
+        console.log(this.previousOperationText.innerText)
+        const previous = +this.previousOperationText.innerText
+        const current = +this.currentOperationText.innerText
+        
+        switch (operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous)
+                break;
+            default:
+                break;
+        }
+    }
+
+    //change values of the calculator screen
+    updateScreen(operationValue = null, operation = null, current = null, previous = 0){
+        console.log(operationValue, operation, current, previous)
+        this.currentOperationText.innerText += this.currentOperation;
+        /*if(operationValue === null){
+                this.currentOperationText.innerText += this.currentOperation;
+        }else{
+            if(previous == 0){
+                operationValue = current;
+            }
+
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = "";
+        }*/
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+            calc.addDigit(value);
+        }else{
+            calc.processOperation(value);
+        }
+    });
+});
